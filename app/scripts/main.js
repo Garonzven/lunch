5337d371d0b6d54e2d7b58235c3532b2df2c4fe8
fgraterols <fabiana.graterol@garonz.com>
2017-04-24 12:31:29 -0400

    Dashboard User changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,43 @@
 $(document).ready(function() {
     $.ajax({
-        url: "login.html",
-        type: "get",
-        dataType: "text",
+        url: 'login.html',
+        type: 'get',
+        dataType: 'text',
         success: function(response) {
           $('.container').html(response);
-          $("#login").on("click", function() {
+          $('#login').on('click', function() {
             $.ajax({
-              url:"http://127.0.0.1:8000/login/signin",
-              method:"post",
-              data: {email:$("#email").val(),password:$("#password").val()},
-              dataType:"JSON",
+              url:'http://127.0.0.1:8000/login/signin',
+              method:'post',
+              data: {email:$('#email').val(),password:$('#password').val()},
+              dataType:'JSON',
               success: function(data){
-                  console.log(data);
+                  $.ajax({
+                    url:'user_dashboard.html',
+                    type:'get',
+                    dataType: 'text',
+                    success: function(data2) {
+                      $('.container').html(data2);
+                      $('#name').html(data.name);
+                      $.ajax({
+                        url:'avatar.html',
+                        type:'get',
+                        dataType:'text',
+                        success:function(response){
+                            $('.avatar').html(response);
+                        }
+                      });
+                    }
+                  });
               }
             });
           });
         }
     });
       // $.ajax({
-      //     url: "user_dashboard.html",
-      //     type: "get",
-      //     dataType: "text",
+      //     url: 'user_dashboard.html',
+      //     type: 'get',
+      //     dataType: 'text',
       //     success: function(response) {
       //         $('.container').html(response);
       //     }
