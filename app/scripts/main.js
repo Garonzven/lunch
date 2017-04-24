d1a08ccfb4e2c39d640fa03a7bba55312755615c
fgraterols <fabiana.graterol@garonz.com>
2017-04-24 09:01:01 -0400

    login integration with backend

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
-    // $.ajax({
-    //     url: "login.html",
-    //     type: "get",
-    //     dataType: "text",
-    //     success: function(response) {
-    //         $('.container').html(response);
-    //     }
-    // });
     $.ajax({
-        url: "user_dashboard.html",
+        url: "login.html",
         type: "get",
         dataType: "text",
         success: function(response) {
-            $('.container').html(response);
+          $('.container').html(response);
+          $("#login").on("click", function() {
+            $.ajax({
+              url:"http://127.0.0.1:8000/login/signin",
+              method:"post",
+              data: {email:$("#email").val(),password:$("#password").val()},
+              dataType:"JSON",
+              success: function(data){
+                  console.log(data);
+              }
+            });
+          });
         }
     });
+      // $.ajax({
+      //     url: "user_dashboard.html",
+      //     type: "get",
+      //     dataType: "text",
+      //     success: function(response) {
+      //         $('.container').html(response);
+      //     }
+      // });
 });
