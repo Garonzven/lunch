dc4bb50eb82e8ed02cfcd509b1ec0f914b4ac0d1
fgraterols <fabiana.graterol@garonz.com>
2017-04-24 15:43:01 -0400

    login

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
-  $("#login").on("click", function() {
-    $.ajax({
-      url: "login.json",
-      method: "get",
-      data: {
-        email: $("#email").val(),
-        password: $("#password").val()
-      },
-      dataType: "JSON",
-      success: function(data) {
-        if (data.token) {
-          $.cookie("token", data.token, { expires: 7 });
-          $(location).attr("href", "user_dashboard.html");
-        } else {
-          alert("Invalid credentials!");
+    $("#myform").validate({
+      rules: {
+        // simple rule, converted to {required:true}
+        password: {
+          required:true
+        },
+        // compound rule
+        email: {
+          required: true,
+          email: true
         }
+      },
+
+      messages:{
+        password:{
+          required: "Please enter your password"
+        },
+        email:{
+          required: "Please enter your email",
+          email: "Please enter a correct email"
+        }
+
+      },
+
+      submitHandler: function(form){
+        $.ajax({
+          url: "login.json",
+          method: "get",
+          data: {
+            email: $("#email").val(),
+            password: $("#password").val()
+          },
+          dataType: "JSON",
+          success: function(data) {
+            if (data.token) {
+              $.cookie("token", data.token, { expires: 7 });
+              $(location).attr("href", "user_dashboard.html");
+            } else {
+              alert("Invalid credentials!");
+            }
+          }
+        });
       }
     });
-  });
 });
