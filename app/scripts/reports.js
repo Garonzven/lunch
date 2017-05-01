e40130a536541c25211702d74587ea4404a5d90d
fgraterols <fabiana.graterol@garonz.com>
2017-05-01 16:55:05 -0400

    profile

---
+++ app/scripts/reports.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#example').dataTable();
+
+  $('.print').on('click',function(){
+    swal({
+      title: 'Are you sure?',
+      text: "There are some lunchers who haven't choose their dishes",
+      imageUrl: 'assets/warning_1.png',
+      imageWidth: 200,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Print'
+    }).then(function () {
+      swal(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    });
+  });
+});
