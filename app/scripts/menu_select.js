a0fb0040d263bb02555d72562574a26fab25b7e3
fgraterols <fabiana.graterol@garonz.com>
2017-05-02 13:30:40 -0400

    select de menus avanzado

--- app/scripts/menu_select.js
+++ app/scripts/menu_select.js
@@ -7,8 +7,18 @@ function formatDate(date) {
 
 function formatId(date){
   var d = new Date(date);
-  var n = d.getMonth()+'.'+d.getDate()+'.'+d.getFullYear();
+  var n = d.getMonth()+'-'+d.getDate()+'-'+d.getFullYear();
   return n;
+}
+
+function addActive(li){
+  id_active = $(li).attr('id');
+
+  $('.ciclo >li.current').removeClass('current');
+  $('.ciclo > li#'+id_active).addClass('current');
+  $('.menus .current').removeClass('current');
+  $('.menu_'+id_active).addClass('current');
+
 }
 
 $('document').ready(function(){
@@ -19,16 +29,23 @@ $('document').ready(function(){
     success: function(data){
     date = "";
       $.each(data.data.dishes,function(key,data){
+        console.log(data);
         if(date != data.date_cycle){
           day = formatDate(data.date_cycle);
           cid = formatId(data.date_cycle);
-          $('.ciclo').append('<li class="'+cid+' ciclo-day">'+formatDate(data.date_cycle)+'<div class="ciclo-status"></div></li>');
+          $('.ciclo').append('<li id="'+cid+'" class="ciclo-day" onClick=" addActive(this)"><a>'+formatDate(data.date_cycle)+'</a></li>');
         }
-        console.log(data);
-         $('.menus').append('<div class="'+cid+' col-sm-6 col-md-4"><div class="thumbnail thumbnail-menu"><div class="caption"><input type="hidden" id="dish" value="'+data.id_dish+'"></input><p class="title-menu">'+data.title+'</p><p class="text-justify thumbnail-desc">'+data.description+' </p><p class="put-bottom"><a href="#" class="btn btn--yellow pull-right" role="button">Select</a> </p></div></div></div>');
+         $('.menus').append('<div class="menu_'+cid+' col-sm-6 col-md-4"><div class="thumbnail thumbnail-menu"><div class="caption"><input type="hidden" id="dish" value="'+data.id_dish+'"></input><p class="title-menu">'+data.title+'</p><p class="text-justify thumbnail-desc">'+data.description+' </p><p class="put-bottom"><a href="#" class="btn btn--yellow pull-right" role="button">Select</a> </p></div></div></div>');
         date = data.date_cycle;
       });
-
+    $('.ciclo').children().first().addClass('current');
+    id_active = $('.ciclo').children().first().attr('id');
+    console.log(id_active);
+    $('.menu_'+id_active).addClass('current');
     }
   });
+
+
+
+
 });
