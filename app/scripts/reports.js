e38c3307b873705ddf6d7c38e8116b3c6a59200a
fgraterols <fabiana.graterol@garonz.com>
2017-05-05 10:46:52 -0400

    reportes listo

--- app/scripts/reports.js
+++ app/scripts/reports.js
@@ -15,7 +15,7 @@ $.ajax({
           dataType: 'text',
           success: function(data) {
             $('.sidebar-nav').html(data);
-            
+
           }
         });
         break;
@@ -44,41 +44,8 @@ $.ajax({
   success: function(data){
     console.log(data);
     $.each(data.data, function (key, data) {
-      $('#report-detail').append('<tr><td align="center">'+formatday(data.initial_date)+'</td><td align="center">'+formatday(data.closing_date)+'</td><td align="center"><button type="button" class="btn btn--yellow print" onClick="printReport(this.id)" name="print" id="'+data.id+'" ><i class="glyphicon glyphicon-download-alt"></i></button></td></tr>');
+      $('#report-detail').append('<tr><td align="center">'+formatday(data.initial_date)+'</td><td align="center">'+formatday(data.closing_date)+'</td><td align="center"><a class="btn btn--yellow print" href="'+constants().cycleReport +'/'+data.id+'?token=' + $.cookie('token')+'" name="print" id="'+data.id+'" ><i class="glyphicon glyphicon-download-alt"></i></a></td></tr>');
     });
   $('#example').dataTable();
   }
 });
-
-function printReport(e){
-  console.log(e);
-
-  $.ajax({
-    url: constants().cycleReport + '?token=' + $.cookie('token'),
-    method:'PUT',
-    data:{
-      id : e
-    },
-    dataType:'JSON',
-    success: function(){
-      console.log(data);
-    }
-  });
-}
-
-$(document).ready(function(){
-
-
-  $('.print').on('click',function(){
-    swal({
-      title: 'Are you sure?',
-      text: 'There are some lunchers who haven\'t choose their dishes',
-      imageUrl: 'assets/warning_1.png',
-      imageWidth: 200,
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Print'
-    });
-    });
-  });
